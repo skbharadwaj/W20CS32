fed9f5238293106db84df83f1273cf35ed1ca7ff
skbharadwaj <shreyas.ca@gmail.com>
2020-02-04 13:43:48 -0800

    Add files via upload

---
+++ Homework 2/eval.cpp
@@ -0,0 +1,206 @@
+#include "Map.h"
+#include <iostream>
+#include <string>
+#include <stack>
+#include <cctype>
+#include <cassert>
+
+using namespace std;
+
+int evaluate(string infix, const Map& values, string& postfix, int& result);
+void toPostFix(string infix, string& postfix);
+bool isOperator(char c);
+bool operand(char c);
+bool precedence(char op1, char op2);
+int valueInMapAt(const Map& m, char key);
+int returnValue(const Map& m, string infix);
+char closest(string infix, int i, bool which);
+
+int main () {
+            char vars[] = { 'a', 'e', 'i', 'o', 'u', 'y', '#' };
+            int  vals[] = {  3,  -9,   6,   2,   4,   1  };
+            Map m;
+            for (int k = 0; vars[k] != '#'; k++)
+                m.insert(vars[k], vals[k]);
+            string pf;
+            int answer;
+            assert(evaluate("a+ e", m, pf, answer) == 0  &&
+                                    pf == "ae+"  &&  answer == -6);
+            answer = 999;
+            assert(evaluate("", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("a+", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("a i", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("ai", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("()", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("()o", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("y(o+u)", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("y(*o)", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("a+E", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("(a+(i-o)", m, pf, answer) == 1  &&  answer == 999);
+              // unary operators not allowed:
+            assert(evaluate("-a", m, pf, answer) == 1  &&  answer == 999);
+            assert(evaluate("a*b", m, pf, answer) == 2  &&
+                                    pf == "ab*"  &&  answer == 999);
+            assert(evaluate("y +o *(   a-u)  ", m, pf, answer) == 0  &&
+                                    pf == "yoau-*+"  &&  answer == -1);
+            answer = 999;
+            assert(evaluate("o/(y-y)", m, pf, answer) == 3  &&
+                                    pf == "oyy-/"  &&  answer == 999);
+            assert(evaluate(" a  ", m, pf, answer) == 0  &&
+                                    pf == "a"  &&  answer == 3);
+            assert(evaluate("((a))", m, pf, answer) == 0  &&
+                                    pf == "a"  &&  answer == 3);
+            cout << "Passed all tests" << endl;
+}
+
+int returnValue(const Map& m, string infix) {
+	for(int i = 0;i < infix.size();i++) {
+		if((int)(infix[i]) >= 65 && (int)(infix[i]) <= 90) return 1;
+	}
+	int open = 0;
+	int close = 0;
+	for(int i = 0;i < infix.size();i++) {
+		if(infix[i] == '(') open++;
+		if(infix[i] == ')') close++;
+	}
+	if(open != close) return 1;
+	if(infix.size() == 0) return 1;
+	for(int i = 0;i < infix.size();i++) 
+		if(operand(infix[i]) && !m.contains(infix[i])) return 2;
+	if(infix.size() == 1 && !operand(infix[0])) return 1;
+	char first = closest(infix, -1, true);
+	int indexFirst = 0;
+	for(int i = 0;i < infix.size();i++)	
+		if(first == infix[i]) { indexFirst = i; break; }
+	char second = closest(infix, indexFirst, true);
+
+	if(isOperator(first) || first == ')' || (second == '(' && operand(first)) || second == ')') return 1;
+	for(int i = 1;i < infix.size();i++) {
+		char ch = infix[i];
+		cout << ch;
+		char before = closest(infix, i, false);
+		cout << " " << before;
+		char after = closest(infix, i, true);
+		cout << " " << after << endl;
+		if(operand(ch)) {
+			if(operand(before) || operand(after)) return 1;
+			else if(before == ')' || after == '(') return 1;
+		}
+		else if(isOperator(ch)) {
+			if(isOperator(before) || isOperator(after)) return 1;
+			else if(before == '(' || after == ')') return 1;
+		}
+	}
+	char end;
+	int index = 0;
+	for(int i = 0;i < infix.size();i++)
+		if(infix[i] != ' ') { end = infix[i]; index = i; }
+	char secondLast = closest(infix, index, false);
+
+	if(isOperator(end) || end == '(') return 1;
+	if((secondLast == ')' && operand(end)) || secondLast == '(') return 1;
+	if(end == '(') return 1;
+	return 0;
+}
+
+char closest(string infix, int i, bool which) {
+	if(which) {
+		for(int j = i+1;j < infix.size();j++) if(infix[j] != ' ') return infix[j];
+	}
+	else { 
+		for(int j = i-1;j > - 1;j--) if(infix[j] != ' ') return infix[j];
+	}
+	return '$';
+}
+
+int valueInMapAt(const Map& m, char key) {
+	ValueType v;
+	m.get(key, v);
+	return v;
+}
+
+int evaluate(string infix, const Map& values, string& postfix, int& result) {
+	toPostFix(infix, postfix);
+	stack<int> operands; 
+	int rv = returnValue(values, infix);
+
+	if(rv == 1 || rv == 2) return rv;
+    for(int i = 0;i < postfix.size();i++) {
+        int op1;
+        int op2;
+        if(operand(postfix[i])) operands.push(valueInMapAt(values, postfix[i]));
+        else {
+            op1 = operands.top();
+            operands.pop();
+            op2 = operands.top();
+            operands.pop();
+            switch(postfix[i]) {
+                case '+':
+                    operands.push(op1 + op2);
+                    break;
+                case '-':
+                    operands.push(op2 - op1);
+                    break;
+                case '*':
+                    operands.push(op1*op2);
+                    break;
+                case '/':
+					if(op1 == 0) return 3;
+                    operands.push(op2/op1);
+                    break;
+            }
+        }
+    }
+    result = operands.top();
+	cout << result << endl;
+	if(operands.size() != 1) return 1;
+	return 0;
+}
+
+void toPostFix(string infix, string& postfix) {
+	postfix = "";
+	stack<char> operators;
+	for(int i = 0;i < infix.size();i++) {
+		char c = infix[i];
+		if(operand(c)) postfix += c;
+		else if(c == '(') operators.push(c);
+		else if(c == ')') {
+			while(operators.top() != '(') {
+				postfix += operators.top();
+				operators.pop();
+			}
+			operators.pop();
+		}
+		else if(isOperator(c)) {
+			while(!operators.empty() && operators.top() != '(' && precedence(c, operators.top())) {
+				postfix += operators.top();
+				operators.pop();
+			}
+			operators.push(c);
+		}
+	}
+	while(!operators.empty()) {
+		postfix += operators.top();
+		operators.pop();
+	}
+}
+
+
+bool isOperator(char c) {
+    return c == '+' || c == '-' || c == '/' || c == '*';
+}
+
+bool operand(char c) { 
+	return !isOperator(c) && c != '(' && c != ')' && c != ' ' && c != '$';
+}
+
+bool precedence(char op1, char op2) {
+	return (op1 == op2) || (op1 == '+') || (op1 == '-') || (op1 == '*' && op2 == '/') || (op1 == '/' && op1 == '*');
+}
+
+
+
+
+
+
+
---
+++ Homework 2/mazestack.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stack>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec);
+
+class Coord	{
+	public:
+		Coord(int rr, int cc) : m_r(rr), m_c(cc) {}
+        int r() const { return m_r; }
+        int c() const { return m_c; } 
+	private:
+        int m_r;
+        int m_c;
+};
+
+int main () {
+            string maze[10] = {
+                "XXXXXXXXXX",
+                "X...X..X.X",
+                "X.XXX....X",
+                "X.X.XXXX.X",
+                "XXX......X",
+                "X...X.XX.X",
+                "X.X.X..X.X",
+                "X.XXXX.X.X",
+                "X..X...X.X",
+                "XXXXXXXXXX"
+            };
+        
+            if (pathExists(maze, 10,10, 4,3, 1,8))
+                cout << "Solvable!" << endl;
+            else
+                cout << "Out of luck!" << endl;
+}
+
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) {
+	stack<Coord> coords;
+	coords.push(Coord(sr,sc));
+	maze[sr][sc] = 'E';
+	while(!coords.empty()) {
+		Coord curr = coords.top();
+		coords.pop();	
+		if(curr.r() == er && curr.c() == ec) return true;
+		if(maze[curr.r()][curr.c()+1] == '.') {
+			coords.push(Coord(curr.r(),curr.c()+1));
+			maze[curr.r()][curr.c()+1] = 'E';
+		}
+		if(maze[curr.r()+1][curr.c()] == '.') {
+      		coords.push(Coord(curr.r()+1,curr.c()));
+            maze[curr.r()+1][curr.c()] = 'E';
+		}
+        if(maze[curr.r()][curr.c() - 1] == '.') {
+            coords.push(Coord(curr.r(),curr.c() - 1));
+            maze[curr.r()][curr.c()-1] = 'E';
+		}
+        if(maze[curr.r()-1][curr.c()] == '.') {
+            coords.push(Coord(curr.r()-1,curr.c()));
+            maze[curr.r()-1][curr.c()] = 'E';
+		}
+	}
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
