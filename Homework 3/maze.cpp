8f51311c83712bfe3a8b9299ac6f025c703a8248
skbharadwaj <shreyas.ca@gmail.com>
2020-02-07 00:29:35 -0800

    Add files via upload

---
+++ Homework 3/maze.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) {
+    if(sr == er && sc == ec) return true;
+    maze[sr][sc] = 'E';
+    if(maze[sr][sc+1] == '.') if(pathExists(maze, nRows, nCols, sr, sc + 1, er, ec)) return true;
+    if(maze[sr+1][sc] == '.') if(pathExists(maze, nRows, nCols, sr + 1, sc, er, ec)) return true;
+    if(maze[sr][sc-1] == '.') if(pathExists(maze, nRows, nCols, sr, sc - 1, er, ec)) return true;
+    if(maze[sr-1][sc+1] == '.') if(pathExists(maze, nRows, nCols, sr - 1, sc, er, ec)) return true;
+    return false;
+}
+
