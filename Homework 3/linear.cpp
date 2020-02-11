197afe0af1663568e85df2d78ddeca359a9b4f3a
skbharadwaj <shreyas.ca@gmail.com>
2020-02-11 13:10:49 -0800

    Add files via upload

--- Homework 3/linear.cpp
+++ Homework 3/linear.cpp
@@ -1,20 +1,22 @@
 bool anyFalse(const double a[], int n) {
         if(n == 0 || n < 0) return false;
-	    if(!predicate(a[n-1])) return true;
+	if(!somePredicate(a[n-1])) return true;
         return anyFalse(a, n-1);
         return false;
 }
+
 int countTrue(const double a[], int n) {
-        if(n == 0) return 0;
-	    if(predicate(a[n-1])) return 1 + countTrue(a, n- 1);
+        if(n <= 0) return 0;
+	if(somePredicate(a[n-1])) return 1 + countTrue(a, n- 1);
         return countTrue(a, n - 1);
 }
-=
+
 int firstTrue(const double a[], int n) {
-        if(n == 0) return -1;
-        if(predicate(a[0])) return 0;
+        if(n <= 0) return -1;
+        if(somePredicate(a[0])) return 0;
         return 1 + firstTrue(a+1,n-1);
 }
+
 int positionOfSmallest(const double a[], int n)
 {
         if(n <= 0) return -1;
@@ -22,12 +24,15 @@ int positionOfSmallest(const double a[], int n)
         if(a[0] < a[n-1]) return positionOfSmallest(a, n - 1);
         else return 1 + positionOfSmallest(a + 1,n);
 }
+
 bool contains(const double a1[], int n1, const double a2[], int n2)
 {
+        if(n1 <= 0) return false;
+        if(n2 <= 0) return true;
         if(a2 + 1 == &a2[n2] && a1[0] == a2[0]) return true;       
         if(a1 + 1 == &a1[n1]) return false;
         if(a1[0] == a2[0]) return contains(a1 + 1, n1 - 1, a2 + 1, n2 - 1);
         else if(a1[0] != a2[0]) return contains(a1+1, n1 - 1, a2, n2);
-	    return true;
+	return true;
 }
 
